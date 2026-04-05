35cf6154fe15a0c7fea70de1d14a4aa7efab09cc
Andreydeveloper84 <tikhonov-a-s@yandex.ru>
2026-04-05 16:20:43 +0300

    fix: restore sequence reset migration for PostgreSQL serviceblock/review tables

---
+++ mysite/services_app/migrations/0034_fix_sequences.py
@@ -0,0 +1,35 @@
+from django.db import migrations
+
+TABLES_TO_FIX = [
+    'services_app_serviceblock',
+    'services_app_review',
+    'services_app_service',
+    'services_app_servicemedia',
+]
+
+RESET_SQL = (
+    "SELECT setval("
+    "    pg_get_serial_sequence('{table}', 'id'),"
+    "    COALESCE((SELECT MAX(id) FROM {table}), 0) + 1,"
+    "    false"
+    ")"
+)
+
+
+def fix_sequences(apps, schema_editor):
+    if schema_editor.connection.vendor != 'postgresql':
+        return
+    with schema_editor.connection.cursor() as cursor:
+        for table in TABLES_TO_FIX:
+            cursor.execute(RESET_SQL.format(table=table))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services_app', '0033_service_emoji_service_related_services_and_more'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_sequences, migrations.RunPython.noop),
+    ]
